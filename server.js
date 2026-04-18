96da8dc50ae9bf9557ce73b070be44037f603557
Dayana <dayana@gmail.com>
2026-04-18 18:10:49 -0500

    cambios

--- server.js
+++ server.js
@@ -67,12 +67,118 @@ function seedDatabase() {
   const img = (id) => `${base}/${id}.png`;
   const back = (id) => `${base}/back/${id}.png`;
   const shiny = (id) => `${base}/shiny/${id}.png`;
-
-  const pokemons = [
-    { id: 2, nombre: "Ivysaur", imagen_frontal: img(2), imagen_posterior: back(2), imagen_shiny: shiny(2), altura: 1, peso: 13, tipo1: "Planta", tipo2: "Veneno" },
-    { id: 5, nombre: "Charmeleon", imagen_frontal: img(5), imagen_posterior: back(5), imagen_shiny: shiny(5), altura: 1.1, peso: 19, tipo1: "Fuego", tipo2: null },
-    { id: 8, nombre: "Wartortle", imagen_frontal: img(8), imagen_posterior: back(8), imagen_shiny: shiny(8), altura: 1, peso: 22.5, tipo1: "Agua", tipo2: null }
-  ];
+const pokemons = [
+  {
+    id: 2,
+    nombre: "Ivysaur",
+    imagen_frontal: img(2),
+    imagen_posterior: back(2),
+    imagen_shiny: shiny(2),
+    altura: 1.0,
+    peso: 13.0,
+    tipo1: "Planta",
+    tipo2: "Veneno"
+  },
+  {
+    id: 5,
+    nombre: "Charmeleon",
+    imagen_frontal: img(5),
+    imagen_posterior: back(5),
+    imagen_shiny: shiny(5),
+    altura: 1.1,
+    peso: 19.0,
+    tipo1: "Fuego",
+    tipo2: null
+  },
+  {
+    id: 8,
+    nombre: "Wartortle",
+    imagen_frontal: img(8),
+    imagen_posterior: back(8),
+    imagen_shiny: shiny(8),
+    altura: 1.0,
+    peso: 22.5,
+    tipo1: "Agua",
+    tipo2: null
+  },
+  {
+    id: 26,
+    nombre: "Raichu",
+    imagen_frontal: img(26),
+    imagen_posterior: back(26),
+    imagen_shiny: shiny(26),
+    altura: 0.8,
+    peso: 30.0,
+    tipo1: "Eléctrico",
+    tipo2: null
+  },
+  {
+    id: 40,
+    nombre: "Wigglytuff",
+    imagen_frontal: img(40),
+    imagen_posterior: back(40),
+    imagen_shiny: shiny(40),
+    altura: 1.0,
+    peso: 12.0,
+    tipo1: "Normal",
+    tipo2: "Hada"
+  },
+  {
+    id: 53,
+    nombre: "Persian",
+    imagen_frontal: img(53),
+    imagen_posterior: back(53),
+    imagen_shiny: shiny(53),
+    altura: 1.0,
+    peso: 32.0,
+    tipo1: "Normal",
+    tipo2: null
+  },
+  {
+    id: 95,
+    nombre: "Onix",
+    imagen_frontal: img(95),
+    imagen_posterior: back(95),
+    imagen_shiny: shiny(95),
+    altura: 8.8,
+    peso: 210.0,
+    tipo1: "Roca",
+    tipo2: "Tierra"
+  },
+  {
+    id: 134,
+    nombre: "Vaporeon",
+    imagen_frontal: img(134),
+    imagen_posterior: back(134),
+    imagen_shiny: shiny(134),
+    altura: 1.0,
+    peso: 29.0,
+    tipo1: "Agua",
+    tipo2: null
+  },
+  {
+    id: 135,
+    nombre: "Jolteon",
+    imagen_frontal: img(135),
+    imagen_posterior: back(135),
+    imagen_shiny: shiny(135),
+    altura: 0.8,
+    peso: 24.5,
+    tipo1: "Eléctrico",
+    tipo2: null
+  },
+  {
+    id: 136,
+    nombre: "Flareon",
+    imagen_frontal: img(136),
+    imagen_posterior: back(136),
+    imagen_shiny: shiny(136),
+    altura: 0.9,
+    peso: 25.0,
+    tipo1: "Fuego",
+    tipo2: null
+  }
+];
 
   const insert = db.prepare(`
     INSERT INTO pokemon VALUES (@id,@nombre,@imagen_frontal,@imagen_posterior,@imagen_shiny,@altura,@peso,@tipo1,@tipo2)
